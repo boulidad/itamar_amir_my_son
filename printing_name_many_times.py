c26cf6ecca3fb7045e873e0087a8258ffb7cc0d2
Yaron Amir <yaronam@capitolis.com>
2020-11-29 20:56:34 +0700

    added limit to number of repititions using variable

--- printing_name_many_times.py
+++ printing_name_many_times.py
@@ -1,14 +1,18 @@
 name = input("please enter your name: ")
+number_limit = 10
 
 print(f"hello {name}")
 
-number = input("how many times do you want to see your name? ")
+input_number = input(f"how many times do you want to see your name? (no more than {number_limit}) ")
 
 
-int_number = int(number)
+int_number = int(input_number)
+
+if int_number>number_limit:
+	print(f"the bnumer {input_number} is too big, it is higher than {number_limit}")
+	exit()
 
 for counter in range(int_number):
 	print(f"{name} {counter+1}")
-
 
 
